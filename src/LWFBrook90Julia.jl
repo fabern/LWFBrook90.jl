66937da0dd883130262a69185e8a6cd90a1db1a4
fabern <service+github@fber.ch>
2021-02-02 11:21:33 +0100

    Configure documentation III

--- src/LWFBrook90Julia.jl
+++ src/LWFBrook90Julia.jl
@@ -3,13 +3,20 @@
 module LWFBrook90Julia
 
 export greet, input_definitions, my_f
-
 include("extra_file.jl")
 
-
+"""
+    greet()
+Return "Hello World!".
+"""
 greet() = print("Hello World!")
 
-function input_definitions(folder, prefix)
+"""
+input_definitions(folder::String, prefix::String)
+Return "Define here input definitions...".
+"""
+function input_definitions(folder::String, prefix::String)
     return("Define here input definitions...")
 end
+
 end # module
--- src/extra_file.jl
+++ src/extra_file.jl
@@ -1,3 +1,8 @@
 # fabian.bernhard@wsl.ch, 2021-02-01
 
+"""
+    my_f(x::Number, y::Number)
+Return `2*x+3*y`.
+"""
+
 my_f(x,y) =2*x+3*y
