1c59d0ea347be7c09d1cc40f9e12eb207069b2a6
fabern <10245680+fabern@users.noreply.github.com>
2021-03-10 01:16:51 +0100

    Increase version number and add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,21 @@ using LWFBrook90
 using Test: @test, @testset
 
 # @testset "Module WAT" begin
-@testset "KKMEAN" begin
+@testset "WAT.KKMEAN" begin
     @test LWFBrook90.WAT.KKMEAN(50.,  50., 1., 1.) ≈ 50
     @test LWFBrook90.WAT.KKMEAN(50.,  50., 1., 5.) ≈ 50
     @test LWFBrook90.WAT.KKMEAN(10., 100., 1., 1.) ≈ 31.6227766
     @test LWFBrook90.WAT.KKMEAN(10., 100., 1., 5.) ≈ 14.6779926
     @test LWFBrook90.WAT.KKMEAN(10., 100., 5., 1.) ≈ 68.1292069
+end
+
+# @testset "Module PET" begin
+@testset "PET.ESAT" begin
+    @test LWFBrook90.PET.ESAT(0)[1] ≈ 0.61078 #(0.61078, 0.044448927)
+    @test LWFBrook90.PET.ESAT(0)[2] ≈ 0.044448927 #(0.61078, 0.044448927)
+    @test LWFBrook90.PET.ESAT(25)[1] ≈ 3.1674898302368564  #(3.1674898302368564, 0.18866467946037985)
+    @test LWFBrook90.PET.ESAT(25)[2] ≈ 0.18866467946037985 #(3.1674898302368564, 0.18866467946037985)
+
+    @test LWFBrook90.PET.PM(150., -0.43, 0.06, 146., 19640.) ≈ 0.58512224
+    @test LWFBrook90.PET.PM(150., 0.30, 0.06, 23.8, 578.) ≈ 14.0411894
 end
