c10c1a06f934b27f30774354a933122611a219a7
fabern <10245680+fabern@users.noreply.github.com>
2022-01-31 12:34:56 +0100

    Streamline soil discretization

--- main_with_isotopes.jl
+++ main_with_isotopes.jl
@@ -47,9 +47,9 @@ uSoil_initial, p = define_LWFB90_p(
     simOption_FLAG_MualVanGen;
     Reset = Reset,
     compute_intermediate_quantities = compute_intermediate_quantities,
-    simulate_isotopes = simulate_isotopes#,
+    simulate_isotopes = simulate_isotopes,
     # soil_output_depths = [-0.35, -0.42, -0.48, -1.05]
-    # soil_output_depths = collect(-0.05:-0.05:-1.1)
+    soil_output_depths = collect(-0.05:-0.05:-1.1)
     );
 
 # using Plots
@@ -195,17 +195,28 @@ savefig(pl2, input_prefix*"_plotRecipe.png")
 
 # # # Plot 2
 # # # http://docs.juliaplots.org/latest/generated/gr/#gr-ref43
-# x = LWFBrook90.RelativeDaysFloat2DateTime.(sol_LWFBrook90.t, input_meteoveg_reference_date)
-# y = cumsum(sol_LWFBrook90.prob.p[1][1].p_THICK)
-# z = sol_LWFBrook90[7 .+ (0:sol_LWFBrook90.prob.p[1][1].NLAYER-1),
-#                     1,
-#                     :]./sol_LWFBrook90.prob.p[1][1].p_THICK;
+x = LWFBrook90.RelativeDaysFloat2DateTime.(sol_LWFBrook90.t, input_meteoveg_reference_date)
+y = cumsum(sol_LWFBrook90.prob.p[1][1].p_THICK)
+n = sol_LWFBrook90.prob.p[1][1].NLAYER
+y_centers = [ 0; cumsum(sol_LWFBrook90.prob.p[1][1].p_THICK)[1:(n-1)] ] +
+    sol_LWFBrook90.prob.p[1][1].p_THICK / 2
+
+z = sol_LWFBrook90[7 .+ (0:sol_LWFBrook90.prob.p[1][1].NLAYER-1),
+                    1,
+                    :]./sol_LWFBrook90.prob.p[1][1].p_THICK;
 # z2 = sol_LWFBrook90[idx_u_vector_isotopes_d18O,1,:];
 # z3 = sol_LWFBrook90[idx_u_vector_isotopes_d2H,1,:];
-# heatmap(x, y, z, yflip = true,
-#         xlabel = "Date",
-#         ylabel = "Depth [mm]",
-#         colorbar_title = "θ [-]")
+
+heatmap(x, y_centers, z, yflip = true,
+        xlabel = "Date",
+        ylabel = "Depth [mm]",
+        colorbar_title = "θ [-]")
+hline!([0; cumsum(sol_LWFBrook90.prob.p[1][1].p_THICK)], yflip = true, xticks = false,
+    color = :black, linestyle = :dot
+    #title = "N_layer = "*string(sol_LWFBrook90.prob.[1][1].NLAYER)
+    )
+hline!(y_centers, yflip = true, xticks = false,
+    color = :blue, linestyle = :dot)
 # heatmap(x, y, z2, yflip = true,
 #         xlabel = "Date",
 #         ylabel = "Depth [mm]",
@@ -214,4 +225,103 @@ savefig(pl2, input_prefix*"_plotRecipe.png")
 #         xlabel = "Date",
 #         ylabel = "Depth [mm]",
 #         colorbar_title = "δ2H [mUr]")
+
+# TODO: edges of cells in heatmap are not entirely correct. Find a way to override heatmap()
+#       where we provide cell edges (n+1) instead of cell centers (n)
+# TODO: e.g. plots_heatmap_edges: @recipe function f(::Type{Val{:plots_heatmap_edges}}, xe, ye, z)
+# TODO: e.g. plots_heatmap_edges:     m, n = size(z.surf)
+# TODO: e.g. plots_heatmap_edges:     x_pts, y_pts = fill(NaN, 6 * m * n), fill(NaN, 6 * m * n)
+# TODO: e.g. plots_heatmap_edges:     fz = zeros(m * n)
+# TODO: e.g. plots_heatmap_edges:     for i in 1:m # y
+# TODO: e.g. plots_heatmap_edges:         for j in 1:n # x
+# TODO: e.g. plots_heatmap_edges:             k = (j - 1) * m + i
+# TODO: e.g. plots_heatmap_edges:             inds = (6 * (k - 1) + 1):(6 * k - 1)
+# TODO: e.g. plots_heatmap_edges:             x_pts[inds] .= [xe[j], xe[j + 1], xe[j + 1], xe[j], xe[j]]
+# TODO: e.g. plots_heatmap_edges:             y_pts[inds] .= [ye[i], ye[i], ye[i + 1], ye[i + 1], ye[i]]
+# TODO: e.g. plots_heatmap_edges:             fz[k] = z.surf[i, j]
+# TODO: e.g. plots_heatmap_edges:         end
+# TODO: e.g. plots_heatmap_edges:     end
+# TODO: e.g. plots_heatmap_edges:     ensure_gradient!(plotattributes, :fillcolor, :fillalpha)
+# TODO: e.g. plots_heatmap_edges:     fill_z := fz
+# TODO: e.g. plots_heatmap_edges:     line_z := fz
+# TODO: e.g. plots_heatmap_edges:     x := x_pts
+# TODO: e.g. plots_heatmap_edges:     y := y_pts
+# TODO: e.g. plots_heatmap_edges:     z := nothing
+# TODO: e.g. plots_heatmap_edges:     seriestype := :shape
+# TODO: e.g. plots_heatmap_edges:     label := ""
+# TODO: e.g. plots_heatmap_edges:     widen --> false
+# TODO: e.g. plots_heatmap_edges:     ()
+# TODO: e.g. plots_heatmap_edges: end
+# TODO: e.g. plots_heatmap_edges: @deps plots_heatmap_edges shape
+# TODO: e.g. plots_heatmap_edges: @shorthands plots_heatmap_edges
+# TODO: e.g. plots_heatmap_edges:
+# TODO: e.g. plots_heatmap_edges: Plots.heatmap(x[1:100], y_centers, z[:,1:100])
+# TODO: e.g. plots_heatmap_edges: Plots.heatmap(x[1:100], y_centers, z[:,1:100])
+# TODO: e.g. plots_heatmap_edges: plot(t = :heatmap, x[1:50], y_centers, z[:,1:50]) # works
+# TODO: e.g. plots_heatmap_edges: plot(t = :plots_heatmap, x[1:50], y_centers, z[:,1:50]) # doesn't work
+# TODO: e.g. plots_heatmap_edges: plot(t = :plots_heatmap_edges, x[1:50], y_centers, z[:,1:50]) # doesn't work either
+
+
 # ####################
+function find_indices(depths_to_read_out_mm, solution)
+    # depths and lower_boundaries must all be positive numbers
+    @assert all(depths_to_read_out_mm .> 0)
+
+    lower_boundaries = cumsum(solution.prob.p[1][1].p_THICK)
+    idx_to_read_out = []
+    for curr_depth_mm in depths_to_read_out_mm
+        # idx_to_read_out = findfirst(curr_depth_mm .<= y)
+        append!(idx_to_read_out, findfirst(curr_depth_mm .<= lower_boundaries))
+    end
+    return idx_to_read_out
+end
+
+function get_auxiliary_variables(solution)
+    p_soil = solution.prob.p[1][1]
+    NLAYER = solution.prob.p[1][2][1]
+    u_SWATI = [solution.u[i][7:(7+NLAYER-1)] for i=1:length(solution.u)]
+    # (u_aux_WETNES, u_aux_PSIM, u_aux_PSITI, u_aux_θ, p_fu_KK) =
+    #         LWFBrook90.KPT.derive_auxiliary_SOILVAR.(u_SWATI, Ref(p_soil)) # Ref fixes scalar argument for broadcasting "."
+    u_aux_WETNES, u_aux_PSIM, u_aux_PSITI, u_aux_θ, p_fu_KK =
+        (fill(NaN, (size(u_SWATI,1), size(u_SWATI[1],1))) for i in 1:5);
+    for t in 1:length(u_SWATI)
+        (u_aux_WETNES[t,:], u_aux_PSIM[t,:], u_aux_PSITI[t,:], u_aux_θ[t,:], p_fu_KK[t,:]) =
+            LWFBrook90.KPT.derive_auxiliary_SOILVAR(u_SWATI[t], p_soil)
+    end
+    # returns arrays of dimenstion (t,z) where t is number of timesteps and z number of computational layers
+    return (hcat(u_SWATI...)', u_aux_WETNES, u_aux_PSIM, u_aux_PSITI, u_aux_θ, p_fu_KK)
+end
+
+function get_θ(depths_to_read_out_mm, solution)
+    (u_SWATI, u_aux_WETNES, u_aux_PSIM, u_aux_PSITI, u_aux_θ, p_fu_KK) =
+        get_auxiliary_variables(solution)
+
+    idx = find_indices(depths_to_read_out_mm, solution)
+
+    return u_aux_θ[:,idx]
+end
+function get_SWATI(depths_to_read_out_mm, solution)
+    (u_SWATI, u_aux_WETNES, u_aux_PSIM, u_aux_PSITI, u_aux_θ, p_fu_KK) =
+        get_auxiliary_variables(solution)
+
+    idx = find_indices(depths_to_read_out_mm, solution)
+
+    return u_SWATI[:,idx]
+end
+
+depth_to_read_out_mm = [10 150 500 1000 1150]
+plot(LWFBrook90.RelativeDaysFloat2DateTime.(sol_LWFBrook90.t, input_meteoveg_reference_date),
+    get_θ(depth_to_read_out_mm, sol_LWFBrook90),
+    labels = string.(depth_to_read_out_mm) .* "mm",
+     xlabel = "Date",
+     ylabel = "θ [-]",
+     legend = :bottomright)
+savefig(input_prefix*"_θ-feinerde-_depths_NLAYER"*string(sol_LWFBrook90.prob.p[1][1].NLAYER)*".png")
+
+plot(x,
+     z[find_indices(depth_to_read_out_mm, sol_LWFBrook90), :]',
+     labels = string.(depth_to_read_out_mm) .* "mm",
+     xlabel = "Date",
+     ylabel = "θ [-]",
+     legend = :bottomright)
+savefig(input_prefix*"_θ-feinUndSteinErde_depths_NLAYER"*string(sol_LWFBrook90.prob.p[1][1].NLAYER)*".png")
