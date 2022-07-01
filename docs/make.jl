01fbf90e12d68b2fc5d083d6dca0832834d25969
fabern <10245680+fabern@users.noreply.github.com>
2022-07-01 14:29:50 +0200

    Setup jupyter notebook generation with Literate.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,48 @@
-using Documenter, LWFBrook90
+using LWFBrook90
+using Documenter
+using Literate
 
+### From Literate.jl (https://github.com/fredrikekre/Literate.jl/blob/master/docs/make.jl):
+if haskey(ENV, "GITHUB_ACTIONS")
+    ENV["JULIA_DEBUG"] = "Documenter"
+end
+
+deployconfig = Documenter.auto_detect_deploy_system()
+Documenter.post_status(deployconfig; type="pending", repo="github.com/fabern/LWFBrook90.jl")
+
+# generate examples
+EXAMPLE = joinpath(@__DIR__, "..", "examples", "example.jl")
+OUTPUT = joinpath(@__DIR__, "src/generated")
+function preprocess(str)
+    str = replace(str, "x = 123" => "y = 321"; count=1)
+    return str
+end
+
+Literate.markdown(EXAMPLE, OUTPUT, preprocess = preprocess)
+Literate.notebook(EXAMPLE, OUTPUT, preprocess = preprocess)
+Literate.script(EXAMPLE, OUTPUT, preprocess = preprocess)
+
+# Replace the link in outputformats.md
+# since that page is not "literated"
+if haskey(ENV, "GITHUB_ACTIONS")
+    folder = Base.CoreLogging.with_logger(Base.CoreLogging.NullLogger()) do
+        Documenter.deploy_folder(
+            deployconfig;
+            repo="github.com/fabern/LWFBrook90.jl",
+            devbranch = "develop",
+            push_preview = true,
+            devurl = "dev",
+        ).subfolder
+    end
+    url = "https://nbviewer.jupyter.org/github/fabern/LWFBrook90.jl/blob/gh-pages/$(folder)/"
+    str = read(joinpath(@__DIR__, "src/outputformats.md"), String)
+    str = replace(str, "[notebook.ipynb](generated/notebook.ipynb)." => "[notebook.ipynb]($(url)generated/notebook.ipynb).")
+    write(joinpath(@__DIR__, "src/outputformats.md"), str)
+end
+
+
+
+### From Documenter.jl:
 DocMeta.setdocmeta!(LWFBrook90, :DocTestSetup, :(using LWFBrook90); recursive=true)
 
 makedocs(;
