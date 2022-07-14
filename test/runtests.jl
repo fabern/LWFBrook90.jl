deea2a43cf26b339f4fb19ff57b7499a4a4187da
fabern <10245680+fabern@users.noreply.github.com>
2022-07-14 17:03:46 +0200

    test: Fix unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,9 +17,9 @@ end
 # https://discourse.julialang.org/t/benchmarking-tests-to-ensure-prs-dont-introduce-regressions/8630/6
 # or then https://github.com/maxbennedich/julia-regression-analysis or ...)
 
+# cd("test")
 include("01-unit-tests.jl")
 
-# cd("test")
 include("02-integration-tests.jl")
 
 include("03-regression-tests.jl")
