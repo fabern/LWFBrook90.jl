3bcec5be53e5ddcb7eb415a291d5853951072bba
Fabian Bernhard <fabian.bernhard@wsl.ch>
2023-10-05 03:03:28 +0200

    docs: Show pwd()

--- examples/scripts/example-script-01.jl
+++ examples/scripts/example-script-01.jl
@@ -30,7 +30,7 @@ using LWFBrook90
 # Define simulation model by reading in system definition and input data from input files
 
 ## Read in input data
-@show pwd() #hide
+@warn pwd() # to debug Documenter.jl #hide
 #src input_path = "../examplesisoBEAdense2010-18-reset-FALSE-input/"; input_prefix = "isoBEAdense2010-18-reset-FALSE";
 input_path = "../examples/DAV2020-full/"; input_prefix = "DAV2020-full";
 ## input_path = "../examples/isoBEAdense2010-18-reset-FALSE-input/"; input_prefix = "isoBEAdense2010-18-reset-FALSE";
