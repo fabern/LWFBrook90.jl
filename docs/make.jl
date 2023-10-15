1482317a127ac5502e2529cc2472d01bc4c7a66e
Fabian Bernhard <10245680+fabern@users.noreply.github.com>
2023-10-15 19:22:17 +0200

    docs: Move example-script back to examples/

--- docs/make.jl
+++ docs/make.jl
@@ -34,9 +34,9 @@ end
     # A non-code markdown normal line
     ## A comment within the code chunk
 OUTPUT_markdown_for_Docs = joinpath(@__DIR__, "src/generated")
-OUTPUT_scripts_for_examp = joinpath(@__DIR__, "../examples/scripts/")
+# OUTPUT_scripts_for_examp = joinpath(@__DIR__, "src/generated")
 
-INPUT_example_script_01 = joinpath(@__DIR__, "src/example-script-01.jl")
+INPUT_example_script_01 = joinpath(@__DIR__, "../examples/scripts/example-script-01.jl")
 Literate.markdown(INPUT_example_script_01, OUTPUT_markdown_for_Docs;   name = "example-script-01", documenter = true, preprocess = update_date)
 # Literate.script(  INPUT_example_script_01, OUTPUT_scripts_for_examp;   name = "example-script-01", documenter = true, preprocess = update_date, keep_comments = true)
 # Literate.notebook(INPUT_example_script_01, OUTPUT_scripts_for_examp;   name = "example-script-01", documenter = true, preprocess = update_date) # TODO: reactivate
