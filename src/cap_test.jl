f2122ee8441bbffd3f0c4f087bb2deb7866ef952
Louis Graup <ljgraup@gmail.com>
2026-03-31 11:13:38 +0200

    Add SavingsCallback for callback-derived variables

--- src/cap_test.jl
+++ src/cap_test.jl
@@ -7,7 +7,7 @@ input_path="./examples/PFY2024-capacitance/";
 model = LWFBrook90.loadSPAC(input_path, input_prefix; simulate_isotopes = false,
     root_distribution = (beta = 0.97091, z_rootMax_m=-1.35462));
 
-simulation = LWFBrook90.setup(model, requested_tspan = (0, 230));
+simulation = LWFBrook90.setup(model, requested_tspan = (0, 3));
 
 LWFBrook90.simulate!(simulation)
 
@@ -16,6 +16,7 @@ sol = simulation.ODESolution;
 timesteps = unique(round.(sol.t));
 dates = LWFBrook90.RelativeDaysFloat2DateTime.(timesteps, simulation.parametrizedSPAC.reference_date);
 
+SWATI = [sol(t).SWATI.mm for t in timesteps];
 PLSTOR = [sol(t).PLSTOR.mm for t in timesteps];
 PLPSI = [sol(t).PLHYD.ψ for t in timesteps];
 PLPSI_pd = [sol(t).accum.cum_pd_plpsi for t in timesteps];
@@ -27,7 +28,6 @@ PLRFI = reduce(hcat, [sol(t).PLRFI.mmday for t in timesteps]);
 
 soil = LWFBrook90.get_soil_(:ψ, simulation);
 flux = LWFBrook90.get_fluxes(simulation);
-flux[225:230, :]
 
 #=
 using Dates, DataFrames, Plots; gr()
@@ -44,3 +44,49 @@ sort!(psi_comp, :t)
 plot(psi_comp.t[305:315], psi_comp.PLPSI[305:315])
 
 =#
+
+# post-processing of new saved_all
+
+out = simulation.saved_all;
+t = out.t;
+
+RWU_fix = [out.saveval[t].RWU for t in 1:length(t)];
+prec_fix = [out.saveval[t].accum.cum_d_prec for t in 1:length(t)];
+
+# compare accumulated meteo with forcing
+meteo = LWFBrook90.get_forcing(simulation);
+all((meteo.prec_mmDay[1:(length(t)-1)] .- prec_fix[1:(length(t)-1)]) .< 1e-3);
+
+using DataFrames
+
+function saved_values_to_dataframe(saved)
+    nt = length(saved.t)
+    all_keys = fieldnames(typeof(saved.saveval[1]))  # keys of named tuple
+
+    data_cols = Dict{Symbol, Any}()
+    data_cols[:t] = saved.t
+
+    for k in all_keys
+        # Extract all values for this key over time
+        vals = [getfield(saved.saveval[i], k) for i in 1:nt]
+
+        if (vals[1] isa AbstractArray)
+            nsub = length(vals[1])
+            colnames = propertynames(vals[1])
+            for j in 1:nsub
+                if length(colnames) > 1
+                    colname = colnames[j]
+                else
+                    colname = Symbol("$(k)_$j")
+                end
+                data_cols[colname] = [v[j] for v in vals]
+            end
+        else
+            data_cols[Symbol(k)] = vals
+        end
+    end
+
+    return DataFrame(data_cols)
+end
+
+flux_fix = saved_values_to_dataframe(simulation.saved_all)
